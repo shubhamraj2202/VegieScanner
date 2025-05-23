1dc6e57d0f701387d6f91eac18e0292f595c54d3
Shubham Raj <shubhamraj2202@gmail.com>
2025-05-23 19:18:39 +0900

    Initial Commit

---
+++ VegieScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VegieScanner
+//
+//  Created by Shubham Raj on 23/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VegieScanner/VegieScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegieScannerApp.swift
+//  VegieScanner
+//
+//  Created by Shubham Raj on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct VegieScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
